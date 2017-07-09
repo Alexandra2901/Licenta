ab9f18e731bd15de208610437504ef73d4f37abd
Alexandra Deaconescu <alexandra.deaconescu29@gmail.com>
2017-07-09 20:45:21 +0300

    fara dubiosenii

--- dashboard/bookRooms/bookRooms.service.js
+++ dashboard/bookRooms/bookRooms.service.js
@@ -3,37 +3,23 @@ angular.module("graduationThesis").factory("BookingsFactory", function($rootScop
 var bookingsObject = {};
 bookingsObject.bookings = [{
       id: 1,
-<<<<<<< HEAD
       checkin: new Date("July 21, 2017"),
       checkout: new Date("July 23, 2017"),
       rooms: ["Room 1"],
-=======
-      checkin: "22/07/2017",
-      checkout: "15/07/2017",
-      rooms: [1,2],
->>>>>>> 050b039974efabeab96d83533abfdfdac12a643a
       name: "Popescu Ana",
       phone: "0745657876"
     },
-    {
-      id: 2,
+    { id: 2,
       checkin: new Date("July 8, 2017"),
       checkout: new Date("July 15, 2017"),
       rooms: ["Room 1, Room 2"],
       name: "Popescu Ana",
       phone: "0745657876"
     },
-    {
-      id: 3,
-<<<<<<< HEAD
+    { id: 3,
       checkin: new Date("July 11, 2017"),
       checkout: new Date("July 16, 2017"),
       rooms: ["Room 2"],
-=======
-      checkin: "08/07/2017",
-      checkout: "15/07/2017",
-      rooms: [1,2],
->>>>>>> 050b039974efabeab96d83533abfdfdac12a643a
       name: "Popescu Ana12",
       phone: "0745657876"
     }
