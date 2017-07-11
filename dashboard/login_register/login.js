6a95698c16eb72d49497f3b1fb40feac0a93165b
Alexandra Deaconescu <alexandra.deaconescu29@gmail.com>
2017-07-11 21:13:01 +0300

    CU login

--- dashboard/login_register/login.js
+++ dashboard/login_register/login.js
@@ -1,18 +1,9 @@
-angular.module('graduationThesis').controller('LoginRegisterController', function($scope, UsersFactory, $mdDialog) {
+angular.module('graduationThesis').controller('LoginRegisterController', function($scope, UsersFactory) {
 
-  $scope.showAdvanced = function(ev) {
-    $mdDialog.show({
-        controller: "LoginRegisterModalController",
-        templateUrl: 'dashboard/login_register_modal/login_register_modal.html',
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose: true,
-        fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
-      })
-      .then(function(answer) {
+  $scope.showHints = true;
+  $scope.user =  {};
 
-      }, function() {
-
-      });
-  };
+  $scope.addUser = function() {
+    UsersFactory.addUser($scope.user);
+  }
 });
