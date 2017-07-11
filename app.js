de4b8a0c53d5a620ff181ce65b325019565e9bd0
Alexandra Deaconescu <alexandra.deaconescu29@gmail.com>
2017-07-11 12:10:23 +0300

    fara login

---
+++ app.js
@@ -0,0 +1,50 @@
+var graduationThesis = angular.module("graduationThesis", ["ui.router", "ngMaterial"]);
+
+graduationThesis.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('login', {
+      name: "login",
+      url: "/",
+      templateUrl: "login_register_modal/login_register_modal.html"
+    })
+    .state('manageRooms', {
+      name: "manageRooms",
+      url: "/manageRooms",
+      templateUrl: "dashboard/manageRooms/manageRooms.html"
+    })
+    .state('specialOffers', {
+      name: "specialOffers",
+      url: "/specialOffers",
+      templateUrl: "dashboard/specialOffers/specialOffers.html"
+    })
+    .state('rates', {
+      name: "rates",
+      url: "/rates",
+      templateUrl: "dashboard/rates/rates.html"
+    })
+    .state('booking', {
+      name: "booking",
+      url: "/booking",
+      templateUrl: "dashboard/bookRooms/bookRooms.html"
+    })
+    .state('reports', {
+      name: "reports",
+      url: "/reports",
+      templateUrl: "dashboard/reports/reports.html"
+    })
+    .state('employees', {
+      name: "employees",
+      url: "/employees",
+      templateUrl: "dashboard/manageEmployees/manageEmployees.html"
+    })
+    .state('editEmployeesPage', {
+      name: "editEmployeesPage",
+      url: "/editEmployeesPage/:idEmployee",
+      templateUrl: "dashboard/employeePage/employee.html"
+    });
+});
