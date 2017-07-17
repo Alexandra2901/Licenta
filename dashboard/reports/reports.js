58b240dc10d99067e144f222c0088edebd246188
Alexandra Deaconescu <alexandra.deaconescu29@gmail.com>
2017-07-17 14:56:20 +0300

    server

--- dashboard/reports/reports.js
+++ dashboard/reports/reports.js
@@ -1,5 +1,22 @@
-angular.module('graduationThesis', ['ngMaterial', 'ngMessages'])
-
-.controller('ReporsController', function($scope){
-
+angular.module("graduationThesis").controller('ReportsController', function($scope, BookingsFactory) {
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  $scope.series = ['Series A'];
+  $scope.data = [65, 59, 80, 81, 56, 55, 40];
+  $scope.onClick = function(points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{
+    yAxisID: 'y-axis-1'
+  }];
+  $scope.options = {
+    scales: {
+      yAxes: [{
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        }
+      ]
+    }
+  };
 })
