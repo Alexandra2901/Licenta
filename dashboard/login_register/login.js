e34cba134dc48970d87493e832e7b1246396a75d
Alexandra Deaconescu <alexandra.deaconescu29@gmail.com>
2017-07-18 23:27:37 +0300

    GATA

--- dashboard/login_register/login.js
+++ dashboard/login_register/login.js
@@ -1,9 +1,17 @@
-angular.module('graduationThesis').controller('LoginRegisterController', function($scope, UsersFactory) {
+angular.module('graduationThesis').controller('LoginRegisterController', function($scope, UsersFactory, $state) {
 
   $scope.showHints = true;
-  $scope.user =  {};
+  $scope.user = {};
 
   $scope.addUser = function() {
     UsersFactory.addUser($scope.user);
+  }
+
+  $scope.login = function() {
+    if ($scope.user.email == "admin@admin.ro" && $scope.user.password == "password") {
+      $state.go('manageRooms');
+    } else {
+      alert('Email or password incorrect!');
+    }
   }
 });
